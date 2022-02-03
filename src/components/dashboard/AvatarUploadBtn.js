4244ece197d3bff4ebba21d35efff61abf78928e
shyam <shyam20998@gmail.com>
2022-02-03 17:52:33 +0530

    displayed user avatar

--- src/components/dashboard/AvatarUploadBtn.js
+++ src/components/dashboard/AvatarUploadBtn.js
@@ -4,6 +4,7 @@ import { Button, Message, Modal, toaster } from 'rsuite';
 import AvatarEditor from 'react-avatar-editor'
 import { database, storage } from '../../misc/firebase';
 import { useProfile } from '../../context/profile.context';
+import ProfileAvatar from './ProfileAvatar';
 
 const fileInputTypes = ".png, .jpeg, .jpg"
 
@@ -85,6 +86,9 @@ const AvatarUploadBtn = () => {
 
     return (
         <div className='mt-3 text-center'>
+            <ProfileAvatar src={profile.avatar} name={profile.name} 
+                className="width-200 height-200 img-fullsize font-huge"/>
+
             <div>
                 <label htmlFor='avatar-upload' className='d-block cursor-pointer padded'>
                     Select new avatar
