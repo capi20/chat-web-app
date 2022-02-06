f8d8cb71f063c668df93f262e8078c335eff02b0
shyam <shyam20998@gmail.com>
2022-02-06 13:57:12 +0530

    started to build chat top; created room description modal

--- src/components/chat-window/top/index.js
+++ src/components/chat-window/top/index.js
@@ -1,10 +1,36 @@
 import React, { memo } from 'react';
 import { useCurrentRoom } from '../../../context/current-room.context';
+import ArrowLeftIcon from '@rsuite/icons/ArrowLeft';
+import { useMediaQuery } from '../../../misc/custom-hooks';
+import { Link } from 'react-router-dom';
+import { ButtonToolbar } from 'rsuite';
+import RoomInfoBtnModal from './RoomInfoBtnModal'
 
 const Top = () => {
-  const name = useCurrentRoom(v => v.name)
+    const name = useCurrentRoom(v => v.name)
 
-  return <div>{name}</div>;
+    const isMobile = useMediaQuery('(max-width: 992px)')
+
+    return (
+      <div>
+        <div className="d-flex justify-content-between align-items-center">
+          <h4>
+            <Link to="/">
+              <ArrowLeftIcon 
+                className={isMobile ? 'd-inline-block p-0 mr-2 text-blue link-unstyled font-mid' : 'd-none'}/>
+            </Link>
+            <span className="text-disappear">{name}</span>
+          </h4>
+
+          <ButtonToolbar className="ws-nowrap">xxx</ButtonToolbar>
+        </div>
+
+        <div className="d-flex justify-content-between align-itmes-center">
+          <span>todo</span>
+          <RoomInfoBtnModal/>
+        </div>
+      </div>
+    )
 };
 
 export default memo(Top);
