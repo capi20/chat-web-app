c102409fb9d960c94e7c0b24fc3e19ec27c9df66
shyam <shyam20998@gmail.com>
2022-02-10 13:26:19 +0530

    added realtime status listener for users

--- src/components/dashboard/DashboardToggle.js
+++ src/components/dashboard/DashboardToggle.js
@@ -2,22 +2,31 @@ import React, { useCallback } from 'react';
 import { Button, Drawer, Message, toaster } from 'rsuite';
 import Dashboard from '.';
 import { useMediaQuery, useModalState } from '../../misc/custom-hooks';
-import { auth } from '../../misc/firebase';
+import { auth, database } from '../../misc/firebase';
 import DashboardIcon from '@rsuite/icons/Dashboard';
+import { isOfflineForDatabase } from '../../context/profile.context';
 
 const DashboardToggle = () => {
     const { isOpen, open, close } = useModalState() 
     const isMobile = useMediaQuery('(max-width: 992px)')
 
     const onSignOut = useCallback(() => {
-        auth.signOut()
+        database.ref(`/status/${auth.currentUser.uid}`).set(isOfflineForDatabase).then(() => {
+            auth.signOut()
 
-        toaster.push(<Message showIcon type="info">Signed out</Message>, {
-            placement: 'topCenter',
-            duration: 4000
-        });
+            toaster.push(<Message showIcon type="info">Signed out</Message>, {
+                placement: 'topCenter',
+                duration: 4000
+            });
+    
+            close()
+        }).catch(err => {
+            toaster.push(<Message showIcon type="error">{err.message}</Message>, {
+                placement: 'topCenter',
+                duration: 4000
+            });
+        })
 
-        close()
     }, [close])
 
     return (
